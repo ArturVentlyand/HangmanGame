7c010fa8d3571ede57c7376b4b55198f7b182f28
ventl <ventl@DESKTOP-S92JBIE>
2022-12-12 12:50:40 +0200

    Добавьте файлы проекта.

---
+++ HangmanGame/GameLogic.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanGame
+{
+    public class GameLogic
+    {
+        private readonly int allowedMisses;
+        private bool[] openIndexes;
+        private int triesCounter = 0;
+        private string triedLetters;
+
+        public GameStatus GameStatus { get; private set; } = GameStatus.NotStarted;
+
+        public string Word { get; private set; }    
+
+        public string TriedLetters
+        {
+            get
+            {
+                var chars = triedLetters.ToCharArray();
+                Array.Sort(chars);
+                return new string(chars);
+            }
+        }
+
+        public int RemainingTries 
+        {
+            get { return allowedMisses - triesCounter; }
+        }
+
+        public GameLogic(int allowedMisses = 6)
+        {
+            if (allowedMisses < 5 || allowedMisses>8)
+            {
+                throw new ArgumentException("Number of misses should be between 5 and 8");
+            }
+
+            this.allowedMisses = allowedMisses;
+        }
+
+        public string GenerateWord()
+        {
+            string[] words = File.ReadAllLines("WordsStockRus.txt");
+            Random random = new Random(DateTime.Now.Millisecond);
+            int randomIndex = random.Next(words.Length - 1);
+
+            Word = words[randomIndex];
+
+            openIndexes = new bool[Word.Length];
+
+            GameStatus = GameStatus.InProgress;
+
+            return Word;
+        }
+
+        public string GuessLetter(char letter)
+        {
+            if (triesCounter == allowedMisses)
+                throw new InvalidOperationException($"Exceeded the max misses number:{allowedMisses}");
+            if (GameStatus != GameStatus.InProgress)
+                throw new InvalidOperationException($"Inaproppriate status of the game:{GameStatus}");
+
+            bool openAny = false;
+            string result = string.Empty;
+            for (int i = 0; i < Word.Length; i++)
+            {
+                if (Word[i] == letter)
+                {
+                    openIndexes[i] = true;
+                    openAny = true;
+                }
+                if (openIndexes[i])
+                    result += Word[i];
+                else
+                    result += "_";
+            }
+
+            if (!openAny)
+                triesCounter++;
+            triedLetters += letter;
+
+            if (IsWin())
+                GameStatus = GameStatus.Won;
+            else if(triesCounter == allowedMisses)
+                GameStatus = GameStatus.Lost;
+
+            return result;
+        }
+
+        private bool IsWin()
+        {
+            foreach (var cur in openIndexes)
+            {
+                if (cur == false)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ HangmanGame/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HangmanGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameLogic game = new GameLogic();
+
+            string word = game.GenerateWord();
+
+            Console.WriteLine($"The word consists of {word.Length} letters.");
+            Console.WriteLine("Try to guess the word.");
+
+            while(game.GameStatus == GameStatus.InProgress)
+            {
+                Console.WriteLine("Pick a letter");
+                char c = Console.ReadLine().ToCharArray()[0];
+
+                string curState = game.GuessLetter(c);
+                Console.WriteLine(curState);
+
+                Console.WriteLine($"Remaining tries = {game.RemainingTries}");
+                Console.WriteLine($"Tried letters:{game.TriedLetters}");
+            }
+
+            if (game.GameStatus == GameStatus.Lost)
+            {
+                Console.WriteLine($"You're hanged.\n" +
+                    $"The word was: {game.Word}");
+            }
+            else if (game.GameStatus == GameStatus.Won)
+            {
+                Console.WriteLine("You Won!");
+            }
+        }
+    }
+}
